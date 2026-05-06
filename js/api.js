94fb95e7c146ee3e9eb4409ddb79cdd6bc0f5ed4
stan-2021131 <sergiocoromac@gmail.com>
2026-05-06 03:09:10 -0600

    Implementacion de funciones para fetch con método post y delete

--- js/api.js
+++ js/api.js
@@ -3,12 +3,32 @@ const CONFIG = {
 }
 
 async function apiGet(endpoint) {
-    try {
-        const res = await fetch(`${CONFIG.API_URL}${endpoint}`)
-        if (!res.ok) throw new Error(`Error en el api ${res.status}`)
-        return res.json()
-    } catch (error) {
-        console.log(error);
-        alert(error.message)
+    const res = await fetch(`${CONFIG.API_URL}${endpoint}`)
+    if (res.status == 404) return { data: [] }
+    if (!res.ok) throw new Error(`Error en el api ${res.status}`)
+    return res.json()
+}
+
+async function apiPost(endpoint, body) {
+    const res = await fetch(`${CONFIG.API_URL}${endpoint}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    if (!res.ok) {
+        throw new Error(`Error en POST ${res.status}`)
+    }
+    return res.json()
+}
+
+async function apiDelete(endpoint) {
+    const res = await fetch(`${CONFIG.API_URL}${endpoint}`, {
+        method: "DELETE"
+    })
+
+    if (!res.ok) {
+        throw new Error(`Error en DELETE ${res.status}`)
     }
 }
